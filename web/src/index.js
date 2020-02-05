2e4c2ad4c4579aa8d02b10aab8ec4c93f1600594
김창수 <kcs1580@naver.com>
2020-02-05 10:44:52 +0900

    [+] edit index.js

--- web/src/index.js
+++ web/src/index.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { createStore } from 'redux';
-import rootReducer from './modules';
-
-import { Provider } from 'react-redux';
-
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-const devTools =
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducer, devTools);
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'));
+  <App />,
+
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
