5c799f28ada94f3a45c0a50a6ca5f1bbfa25380d
김창수 <kcs1580@naver.com>
2020-02-06 09:09:35 +0900

    [+] socket 요청url aws 주소로 변경

--- web/src/pages/sockettest.jsx
+++ web/src/pages/sockettest.jsx
@@ -1,37 +1,46 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import socketio from "socket.io-client";
-
 
 // let num = 1;
 
-const socket = socketio.connect("http://localhost:3001");
+const socket = socketio.connect("http://13.124.177.255:3001");
 (() => {
-    console.log("hi");
-  socket.emit("joinRoom",{roomName:'myroom'});
+  console.log("hi");
+  socket.emit("joinRoom", { roomName: "myroom" });
   socket.on();
-  
 })();
 
 const Sockettest = () => {
-    const [num, setNum] = useState(0);
- 
-  
-  const handle = (e) => {
-      setNum(e.target.value)
-  }
+  const [num, setNum] = useState(0);
 
-  const send = (text) => {
-    socket.emit("reqMsg", {orderNum:text, isReady:false});
-  }
+  const handle = e => {
+    setNum(e.target.value);
+  };
+
+  const send = text => {
+    socket.emit("reqMsg", { orderNum: text, isReady: false });
+  };
 
   return (
     <div>
-        <input type="text" onChange={(e) => {handle(e)}}/ >
-        
-        <button onClick={() => {send(num)}}> 보내기 </button>
+      <input
+        type="text"
+        onChange={e => {
+          handle(e);
+        }}
+      />
+
+      <button
+        onClick={() => {
+          send(num);
+        }}
+      >
+        {" "}
+        보내기{" "}
+      </button>
       <div>{num}</div>
 
-      <br/>
+      <br />
     </div>
   );
 };
