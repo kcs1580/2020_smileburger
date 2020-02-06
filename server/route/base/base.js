a336369109844e61b44f1317b22c5292928f8393
김창수 <kcs1580@naver.com>
2020-02-06 13:30:10 +0900

    [+] add select order

--- server/route/base/base.js
+++ server/route/base/base.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   host: "ssafy-kiosk-db.cpwfrvk3u3vz.us-east-2.rds.amazonaws.com",
   user: "admin",
   password: "adminadmin1234",
-  database: "tmpDB"
+  database: "kiosk"
 });
 mybatisMapper.createMapper(["./sql/base/base.xml"]);
 ////////////////////////////////////////////////////////////////////
@@ -33,7 +33,10 @@ app.get("/test/", function(req, res) {
       pqty: item.qty
     };
     //Send Queries
-    var format = { language: "sql", indent: "  " };
+    var format = {
+      language: "sql",
+      indent: "  "
+    };
     var query = mybatisMapper.getStatement("BASE", "insertTest", selectParams, format);
     connection.query(query, function(error, results, fields) {
       if (error) {
@@ -43,8 +46,7 @@ app.get("/test/", function(req, res) {
     });
   });
   connection.end();
-  //res.send(req.query);
-  //res.json(req.query); // 해당 값 다시 해당 페이지로 보내보기
+  //res.send(req.query); res.json(req.query);  해당 값 다시 해당 페이지로 보내보기
 });
 app.post("/", function(req, res) {
   res.json({ success: "post call succeed!", url: req.url, body: req.body });
@@ -56,6 +58,43 @@ app.put("/", function(req, res) {
 
 app.delete("/", function(req, res) {
   res.json({ success: "delete call succeed!", url: req.url });
+});
+
+app.get("/getOrder", function(req, res) {
+  console.log("들어옴");
+  //connection.connect(); 조회할 파라미터
+  var param = {
+    oid: req.body.oid,
+    ouser: req.body.ouser,
+    ostore: req.body.ostore,
+    odate: req.body.odate,
+    oproducts: req.body.oproducts,
+    ototal: req.body.ototal,
+    otype: req.body.otype,
+    description: req.body.description
+  };
+
+  // 쿼리문 형식
+  let format = {
+    language: "sql",
+    indent: "  "
+  };
+  //첫번째는 xml의 namespace, 두번째는 해당 xml id값, 세번째는 파라미터, 마지막은 포맷.
+  let query = mybatisMapper.getStatement("BASE", "getOrder", param, format);
+  console.log(query); // 쿼리 출력
+
+  connection.query(query, function(error, results, fields) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+  });
+
+  // connection.execute(query, [], function(err, result) {   if (err) {
+  // console.error(err.message);     doRelease(connection);     return;   }
+  // console.log(result.rows);  데이터   doRelease(res, connection, result.rows);
+  // Connection 해제 }); connection.end();
+  res.json({ success: query + " load succeed!", url: req.url });
 });
 
 module.exports = app;
