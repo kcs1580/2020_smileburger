3c27547c43ec47df5bc437bc1baa180fd82e41de
방준영 <youngblood0225@gmail.com>
2020-02-10 17:54:43 +0900

    S02P13C103-73[+] Completed modal function(Smile but no resgisteration user)

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { ModalProvider } from "react-modal-hook";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ModalProvider>
+    <App />
+  </ModalProvider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
--- web/src/pages/auth/auth-go.jsx
+++ web/src/pages/auth/auth-go.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import React, { useState, useMemo } from "react";
+import { Redirect, Route } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import Layout from "../../layout/Layout";
 import Webcam from "react-webcam";
 import Popup from "reactjs-popup";
 import Content from "../../content/content";
+import Order from "../../pages/customer/kiosk-order";
 var AWS = require("aws-sdk");
 const videoConstraints = {
   width: 1280,
@@ -55,6 +61,7 @@ const AuthPage = props => {
     console.log(imageSrc);
     trackEmotions();
   }, [webcamRef]);
+  function registerUser() {}
   function getBinary(encodedFile) {
     var base64Image = encodedFile.split("data:image/jpeg;base64,")[1];
     var binaryImg = atob(base64Image);
@@ -89,7 +96,7 @@ const AuthPage = props => {
       } else {
         console.log(data);
         if (data && data.FaceMatches && data.FaceMatches.length) {
-          console.log("면상 일치한다!!");
+          console.log("속보) Collection에 담긴 FaceID와 면상 일치");
           setIsMatched(true);
           // console.log(fill_metadata(data.FaceMatches));
           console.log(data.FaceMatches);
@@ -110,81 +117,114 @@ const AuthPage = props => {
         console.log("얼굴 인식 ERROR");
         console.log(err);
       } else {
-        console.log(data);
-        console.log(data.FaceDetails[0].Smile);
+        // console.log(data);
+        // console.log(data.FaceDetails[0].Smile);
         var smile = data.FaceDetails[0].Smile;
 
-        console.log(smile.Value);
+        // console.log(smile.Value);
+        console.log("========매칭 된 얼굴 다 가져와라===========");
+        console.log(data.FaceDetails);
+        //onTimeout();
         if (smile.Value === true) {
+          console.log("너는 웃고 있다..");
           setIssmile(true);
           onTimeout();
-
-          // if (isMatched === false) {
-          //   setIsMatched(false);
-          // }
-          console.log("너는 웃고 있다..");
-          //페이지 이동.
         } else {
-          console.log("다시 웃어주세요...ㅠ ㅠㅠㅠ 그래야 님 주문 할 수 있어 ㅋ");
+          console.log("웃어달라고.. 웃어야 그래야 님 주문 할 수 있어 ㅋ");
         }
       }
     });
   };
-  const ChangeComp = () => {
-    if (isMatched) {
-      return (
-        <>
-          <Redirect to="/Order" />
-        </>
-      );
-    } else if (!isSmile) {
-      return (
-        <Layout>
-          <Webcam
-            audio={false}
-            height={720}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            width={1280}
-            videoConstraints={videoConstraints}
-          />
-          <br></br>
-          <Button color="primary" variant="contained" onClick={capture}>
-            Capture photo
-          </Button>
-        </Layout>
-      );
-    } else {
-      return (
-        <Layout>
-          <Webcam
-            audio={false}
-            height={720}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            width={1280}
-            videoConstraints={videoConstraints}
-          />
-          <br></br>
-          <Button color="primary" variant="contained" onClick={capture}>
-            Capture photo
-          </Button>
-        </Layout>
-      );
-    }
+  // ==========================================================
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
   };
-  return <ChangeComp />;
-  // <Layout>
-  //   <Webcam
-  //     audio={false}
-  //     height={720}
-  //     ref={webcamRef}
-  //     screenshotFormat="image/jpeg"
-  //     width={1280}
-  //     videoConstraints={videoConstraints}
-  //   />
-  //   <button onClick={capture}>Capture photo</button>
-  // </Layout>
+
+  const handleClose = () => {
+    setOpen(false);
+    setNewFace(false);
+  };
+  const [newFace, setNewFace] = useState(false);
+  useMemo(() => {
+    if (!isMatched && isSmile) {
+      setNewFace(true);
+      console.log("조건 만족!");
+    }
+    console.log(newFace);
+  }, [newFace, isMatched, isSmile]);
+  // ==========================================================
+  console.log(newFace);
+  return (
+    <>
+      {!isSmile ? (
+        <Layout>
+          <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+          />
+          <br></br>
+          <Button color="primary" variant="contained" onClick={capture}>
+            Capture photo
+          </Button>
+          <Button color="primary" variant="contained" onClick={registerUser}>
+            Capture photo
+          </Button>
+        </Layout>
+      ) : isMatched ? (
+        <Route to="/order" component={Order} />
+      ) : (
+        // <>{() => setOpen(true)}</>
+        <Layout>
+          <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+          />
+          <br></br>
+          <Button color="primary" variant="contained" onClick={capture}>
+            Capture photo
+          </Button>
+          <Button color="primary" variant="contained" onClick={registerUser}>
+            Capture photo
+          </Button>
+        </Layout>
+      )}
+      {/* <ChangeComp /> */}
+      {/* 새로만든 다이얼 */}
+      <Dialog
+        open={newFace}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to
+            Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* 여기까지 */}
+    </>
+  );
 };
 
 export default AuthPage;
