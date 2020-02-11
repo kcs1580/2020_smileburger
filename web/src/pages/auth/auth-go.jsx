76becf1fb9bb4eec978abe98c0c2eaa0f07545ea
방준영 <youngblood0225@gmail.com>
2020-02-11 17:50:26 +0900

    S02P13C103-77 [+] Completed modal windows, ongoing new face registeration

--- web/src/pages/auth/auth-go.jsx
+++ web/src/pages/auth/auth-go.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from "react";
-import { Redirect, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -8,8 +8,6 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Layout from "../../layout/Layout";
 import Webcam from "react-webcam";
-import Popup from "reactjs-popup";
-import Content from "../../content/content";
 import Order from "../../pages/customer/kiosk-order";
 var AWS = require("aws-sdk");
 const videoConstraints = {
@@ -17,6 +15,7 @@ const videoConstraints = {
   height: 720,
   facingMode: "user"
 };
+// Define AWS Resources ================================================
 const cfg = {
   region: "us-west-2",
   upload_bucket_name: "rekognition-stack-demo-s3upload-1gezpp4hs4cul",
@@ -27,7 +26,7 @@ const cfg = {
 var bucketName = cfg.upload_bucket_name;
 var face_collection = cfg.face_collection;
 var table = cfg.ddb_table;
-// Define AWS Resources
+
 var region = cfg.region;
 var creds = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: cfg.identity_pool_id
@@ -37,7 +36,7 @@ AWS.config.update({
   region: region,
   credentials: creds
 });
-
+// Define AWS Resources ==================================================
 var rekognition = new AWS.Rekognition({ apiVersion: "2016-06-27" });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -50,18 +49,34 @@ var s3 = new AWS.S3({
   apiVersion: "2006-03-01",
   params: { Bucket: bucketName }
 });
+
 const AuthPage = props => {
   const [isMatched, setIsMatched] = useState(false);
   const [isSmile, setIssmile] = useState(false);
-  const index = 0;
+
   var imageSrc;
   const webcamRef = React.useRef(null);
-  const capture = React.useCallback(() => {
-    imageSrc = getBinary(webcamRef.current.getScreenshot());
+  var curImg;
+  async function capture() {
+    curImg = webcamRef.current.getScreenshot();
+    imageSrc = getBinary(curImg);
     console.log(imageSrc);
-    trackEmotions();
-  }, [webcamRef]);
-  function registerUser() {}
+    await trackEmotions();
+  }
+
+  async function registerUser() {
+    curImg = webcamRef.current.getScreenshot();
+    imageSrc = getBinary(curImg);
+
+    var params = {
+      CollectionId: face_collection,
+      Image: {
+        Bytes: imageSrc
+      }
+    
+    await 
+  };
+
   function getBinary(encodedFile) {
     var base64Image = encodedFile.split("data:image/jpeg;base64,")[1];
     var binaryImg = atob(base64Image);
@@ -78,7 +93,7 @@ const AuthPage = props => {
 
     return ab;
   }
-  function onTimeout() {
+  async function onTimeout() {
     console.log("얼굴 탐색 시작한다..");
 
     var paramsForFace = {
@@ -89,22 +104,28 @@ const AuthPage = props => {
       },
       MaxFaces: 10
     };
-    rekognition.searchFacesByImage(paramsForFace, function(err, data) {
-      if (err) {
-        //다시 interval가자.
-        console.log("error낫디" + err);
-      } else {
-        console.log(data);
-        if (data && data.FaceMatches && data.FaceMatches.length) {
-          console.log("속보) Collection에 담긴 FaceID와 면상 일치");
-          setIsMatched(true);
-          // console.log(fill_metadata(data.FaceMatches));
-          console.log(data.FaceMatches);
+
+    try {
+      const data = await rekognition.searchFacesByImage(paramsForFace).promise();
+      // await >> try/catch
+      console.log(data);
+      if (data.FaceMatches && data.FaceMatches.length) {
+        console.log("속보) Collection에 담긴 FaceID와 면상 일치");
+        try {
+          await setIsMatched(true).promise();
+        } catch (e) {
+          console.log(e);
         }
+      } else {
+        //interval
       }
-    });
+    } catch (e) {
+      console.log(e);
+      //interval
+    }
   }
-  const trackEmotions = () => {
+
+  async function trackEmotions() {
     console.log("얼굴 분석 들어갑니다.");
     var params = {
       Attributes: ["ALL"],
@@ -112,31 +133,33 @@ const AuthPage = props => {
         Bytes: imageSrc
       }
     };
-    rekognition.detectFaces(params, function(err, data) {
+    await rekognition.detectFaces(params, function(err, data) {
       if (err) {
         console.log("얼굴 인식 ERROR");
         console.log(err);
       } else {
-        // console.log(data);
         // console.log(data.FaceDetails[0].Smile);
         var smile = data.FaceDetails[0].Smile;
 
         // console.log(smile.Value);
-        console.log("========매칭 된 얼굴 다 가져와라===========");
+        console.log("========인식된 얼굴 다 가져와라===========");
         console.log(data.FaceDetails);
-        //onTimeout();
-        if (smile.Value === true) {
-          console.log("너는 웃고 있다..");
-          setIssmile(true);
-          onTimeout();
-        } else {
-          console.log("웃어달라고.. 웃어야 그래야 님 주문 할 수 있어 ㅋ");
-        }
+        //반복문 전부 돌면서 한명이라도 웃고 있으면 이제 faceSearchByImage.
+        data.FaceDetails.some(p => {
+          console.log(p);
+          if (p.Smile.Value === true) {
+            setIssmile(true);
+            onTimeout();
+            return p.Smile.Value === true;
+          } else {
+            console.log("웃어달라고.. 웃어야 그래야 님 주문 할 수 있어 ㅋ");
+          }
+        });
       }
     });
-  };
+  }
   // ==========================================================
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -146,36 +169,43 @@ const AuthPage = props => {
     setOpen(false);
     setNewFace(false);
   };
+
+  const registerUser = () => {
+    curImg = webcamRef.current.getScreenshot();
+    imageSrc = getBinary(curImg);
+    console.log(imageSrc);
+  };
   const [newFace, setNewFace] = useState(false);
   useMemo(() => {
     if (!isMatched && isSmile) {
       setNewFace(true);
       console.log("조건 만족!");
     }
-    console.log(newFace);
-  }, [newFace, isMatched, isSmile]);
+  }, [isMatched, isSmile]);
   // ==========================================================
-  console.log(newFace);
+
   return (
     <>
       {!isSmile ? (
-        <Layout>
+        <body>
           <Webcam
             audio={false}
-            height={720}
+            height={620}
             ref={webcamRef}
             screenshotFormat="image/jpeg"
-            width={1280}
+            width={1080}
             videoConstraints={videoConstraints}
           />
           <br></br>
-          <Button color="primary" variant="contained" onClick={capture}>
-            Capture photo
-          </Button>
-          <Button color="primary" variant="contained" onClick={registerUser}>
-            Capture photo
-          </Button>
-        </Layout>
+          <div style={{ textAlign: "center" }}>
+            <Button color="primary" variant="contained" onClick={capture}>
+              Capture photo
+            </Button>
+            <Button color="primary" variant="contained" onClick={registerUser}>
+              Capture photo
+            </Button>
+          </div>
+        </body>
       ) : isMatched ? (
         <Route to="/order" component={Order} />
       ) : (
@@ -183,10 +213,10 @@ const AuthPage = props => {
         <Layout>
           <Webcam
             audio={false}
-            height={720}
+            height={360}
             ref={webcamRef}
             screenshotFormat="image/jpeg"
-            width={1280}
+            width={720}
             videoConstraints={videoConstraints}
           />
           <br></br>
@@ -206,19 +236,23 @@ const AuthPage = props => {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">{"얼굴 등록 하시겠어요?"}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous location data to
-            Google, even when no apps are running.
-          </DialogContentText>
+          <Webcam
+            audio={false}
+            height={200}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={400}
+            videoConstraints={videoConstraints}
+          />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Disagree
+          <Button onClick={registerUser} color="primary">
+            회원 등록
           </Button>
           <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
+            비회원 주문
           </Button>
         </DialogActions>
       </Dialog>
--- web/src/pages/auth/auth-go2.jsx
+++ web/src/pages/auth/auth-go2.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { Route } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import Layout from "../../layout/Layout";
 import Webcam from "react-webcam";
+import Order from "../../pages/customer/kiosk-order";
 var AWS = require("aws-sdk");
 const videoConstraints = {
   width: 1280,
   height: 720,
   facingMode: "user"
 };
+// Define AWS Resources ================================================
 const cfg = {
   region: "us-west-2",
   upload_bucket_name: "rekognition-stack-demo-s3upload-1gezpp4hs4cul",
@@ -17,7 +26,7 @@ const cfg = {
 var bucketName = cfg.upload_bucket_name;
 var face_collection = cfg.face_collection;
 var table = cfg.ddb_table;
-// Define AWS Resources
+
 var region = cfg.region;
 var creds = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: cfg.identity_pool_id
@@ -27,25 +36,80 @@ AWS.config.update({
   region: region,
   credentials: creds
 });
-
+// Define AWS Resources ==================================================
 var rekognition = new AWS.Rekognition({ apiVersion: "2016-06-27" });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 var identityId = AWS.config.credentials.identityId;
-
+var fill_metadata = function(FaceMatches) {
+  var temp_faces = [];
+  var i;
+};
 var s3 = new AWS.S3({
   apiVersion: "2006-03-01",
   params: { Bucket: bucketName }
 });
+
 const AuthPage = props => {
-  const index = 0;
+  const [isMatched, setIsMatched] = useState(false);
+  const [isSmile, setIssmile] = useState(false);
+
   var imageSrc;
   const webcamRef = React.useRef(null);
+
   const capture = React.useCallback(() => {
-    imageSrc = webcamRef.current.getScreenshot();
+    imageSrc = getBinary(webcamRef.current.getScreenshot());
     console.log(imageSrc);
+    trackEmotions();
   }, [webcamRef]);
+
+  function registerUser() {}
+
+  function getBinary(encodedFile) {
+    var base64Image = encodedFile.split("data:image/jpeg;base64,")[1];
+    var binaryImg = atob(base64Image);
+    var length = binaryImg.length;
+    var ab = new ArrayBuffer(length);
+    var ua = new Uint8Array(ab);
+    for (var i = 0; i < length; i++) {
+      ua[i] = binaryImg.charCodeAt(i);
+    }
+
+    var blob = new Blob([ab], {
+      type: "image/jpeg"
+    });
+
+    return ab;
+  }
+  function onTimeout() {
+    console.log("얼굴 탐색 시작한다..");
+
+    var paramsForFace = {
+      CollectionId: face_collection,
+      FaceMatchThreshold: 80,
+      Image: {
+        Bytes: imageSrc
+      },
+      MaxFaces: 10
+    };
+    rekognition.searchFacesByImage(paramsForFace, function(err, data) {
+      if (err) {
+        //다시 interval가자.
+        console.log("error낫디" + err);
+      } else {
+        console.log(data);
+        if (data && data.FaceMatches && data.FaceMatches.length) {
+          console.log("속보) Collection에 담긴 FaceID와 면상 일치");
+          setIsMatched(true);
+          // console.log(fill_metadata(data.FaceMatches));
+          console.log(data.FaceMatches);
+        }
+      }
+    });
+  }
+
   const trackEmotions = () => {
+    console.log("얼굴 분석 들어갑니다.");
     var params = {
       Attributes: ["ALL"],
       Image: {
@@ -55,26 +119,117 @@ const AuthPage = props => {
     rekognition.detectFaces(params, function(err, data) {
       if (err) {
         console.log("얼굴 인식 ERROR");
+        console.log(err);
       } else {
-        console.log(data);
-        console.log(data.FaceDetails[0].Emotions);
+        // console.log(data.FaceDetails[0].Smile);
+        var smile = data.FaceDetails[0].Smile;
+
+        // console.log(smile.Value);
+        console.log("========인식된 얼굴 다 가져와라===========");
+        console.log(data.FaceDetails);
+        //반복문 전부 돌면서 한명이라도 웃고 있으면 이제 faceSearchByImage.
+        data.FaceDetails.some(p => {
+          console.log(p);
+          if (p.Smile.Value === true) {
+            setIssmile(true);
+            onTimeout();
+            return p.Smile.Value === true;
+          } else {
+            console.log("웃어달라고.. 웃어야 그래야 님 주문 할 수 있어 ㅋ");
+          }
+        });
       }
     });
-
-    return (
-      <Layout>
-        <Webcam
-          audio={false}
-          height={720}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          width={1280}
-          videoConstraints={videoConstraints}
-        />
-        <button onClick={capture}>Capture photo</button>
-      </Layout>
-    );
   };
+  // ==========================================================
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setNewFace(false);
+  };
+  const [newFace, setNewFace] = useState(false);
+  useMemo(() => {
+    if (!isMatched && isSmile) {
+      setNewFace(true);
+      console.log("조건 만족!");
+    }
+  }, [newFace, isMatched, isSmile]);
+  // ==========================================================
+
+  return (
+    <>
+      {!isSmile ? (
+        <Layout>
+          <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+          />
+          <br></br>
+          <Button color="primary" variant="contained" onClick={capture}>
+            Capture photo
+          </Button>
+          <Button color="primary" variant="contained" onClick={registerUser}>
+            Capture photo
+          </Button>
+        </Layout>
+      ) : isMatched ? (
+        <Route to="/order" component={Order} />
+      ) : (
+        // <>{() => setOpen(true)}</>
+        <Layout>
+          <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+          />
+          <br></br>
+          <Button color="primary" variant="contained" onClick={capture}>
+            Capture photo
+          </Button>
+          <Button color="primary" variant="contained" onClick={registerUser}>
+            Capture photo
+          </Button>
+        </Layout>
+      )}
+      {/* <ChangeComp /> */}
+      {/* 새로만든 다이얼 */}
+      <Dialog
+        open={newFace}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to
+            Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* 여기까지 */}
+    </>
+  );
 };
 
 export default AuthPage;
